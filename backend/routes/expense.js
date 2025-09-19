16cbcbb91bbb9d7e6fa5900bfc9a62dc36c1b639
karpal <ilham.nurdian84@gmail.com>
2025-09-19 15:38:46 +0800

    Update expense.js

--- backend/routes/expense.js
+++ backend/routes/expense.js
@@ -1,1 +1,25 @@
+// backend/routes/expense.js
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
+
+router.get('/', (req, res) => {
+  db.all('SELECT * FROM expense ORDER BY date DESC', [], (err, rows) => {
+    if (err) res.status(500).send(err);
+    else res.json(rows);
+  });
+});
+
+router.post('/', (req, res) => {
+  const { date, type, amount, note } = req.body;
+  db.run('INSERT INTO expense (date, type, amount, note) VALUES (?, ?, ?, ?)',
+    [date, type, amount, note],
+    function(err) {
+      if (err) res.status(500).send(err);
+      else res.json({ id: this.lastID });
+    }
+  );
+});
+
+module.exports = router;
 
