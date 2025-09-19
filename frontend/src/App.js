920be16ed84f8583197bb1948dad92ee2c823a45
karpal <ilham.nurdian84@gmail.com>
2025-09-19 16:02:26 +0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import Dashboard from './components/Dashboard';
+import IncomeForm from './components/IncomeForm';
+import ExpenseForm from './components/ExpenseForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Aplikasi Keuangan Ayam Potong</h1>
+      <IncomeForm />
+      <ExpenseForm />
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
 
